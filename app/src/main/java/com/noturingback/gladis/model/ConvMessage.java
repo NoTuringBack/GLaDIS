3e6f44708d5f1d0a1852781a2ebb7766116258d1
vrong <getvrong@gmail.com>
2016-12-02 02:30:57 +0100

    Added GLaDIS communication interface

--- app/src/main/java/com/noturingback/gladis/model/ConvMessage.java
+++ app/src/main/java/com/noturingback/gladis/model/ConvMessage.java
@@ -1,5 +1,6 @@
 package com.noturingback.gladis.model;
 
+import android.graphics.Bitmap;
 import android.media.Image;
 
 /**
@@ -12,6 +13,49 @@ public class ConvMessage
 		Human, Robot
 	}
 	Author author;
-	Image image;
+	Bitmap image = null;
 	String text;
+
+
+	public ConvMessage(Author aut, String text)
+	{
+		this.author = aut;
+		this.text = text;
+	}
+
+	public ConvMessage(Author aut, String text, Bitmap bitmap)
+	{
+		this(aut, text);
+		this.image = bitmap;
+	}
+
+	public Author getAuthor()
+	{
+		return author;
+	}
+
+	public void setAuthor(Author author)
+	{
+		this.author = author;
+	}
+
+	public Bitmap getImage()
+	{
+		return image;
+	}
+
+	public void setImage(Bitmap image)
+	{
+		this.image = image;
+	}
+
+	public String getText()
+	{
+		return text;
+	}
+
+	public void setText(String text)
+	{
+		this.text = text;
+	}
 }
--- app/src/main/java/com/noturingback/gladis/view/MainActivity.java
+++ app/src/main/java/com/noturingback/gladis/view/MainActivity.java
@@ -2,16 +2,58 @@ package com.noturingback.gladis.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.noturingback.gladis.R;
+import com.noturingback.gladis.model.ConvMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.support.v7.appcompat.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
+
+	ListView messageListView;
+	EditText entryEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+		messageListView = (ListView)findViewById(R.id.message_listview);
+		entryEdit = (EditText)findViewById(R.id.message_edit);
+
+		List<ConvMessage> list = new ArrayList<>();
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bonjour"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Comment ça va ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bien et toi ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Nickel"));
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bonjour"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Comment ça va ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bien et toi ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Nickel"));
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bonjour"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Comment ça va ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Robot, "Bien et toi ?"));
+		list.add(new ConvMessage(ConvMessage.Author.Human, "Nickel"));
+
+		messageListView.setAdapter(new ConversationAdapter(this, R.layout.message_item_list, list));
     }
+
+	public void buttonSendAction(View view)
+	{
+
+	}
+
+	public void buttonVoiceAction(View view)
+	{
+
+	}
 
 
 }
